0815e998d2320b3e0a16b7b4a681e13489e384e8
Augustin Briolon <augustin.briolon@fortuneo.com>
2025-02-05 11:06:25 +0100

    ajout de nouvelles fonctions Lambda pour récupérer les restaurants et les avis, mise à jour de la configuration et des dépendances

--- amplify/backend/function/getRestaurants/src/index.js
+++ amplify/backend/function/getRestaurants/src/index.js
@@ -1,23 +1,46 @@
 /* Amplify Params - DO NOT EDIT
-	ENV
-	REGION
-	STORAGE_RESTAURANTS_ARN
-	STORAGE_RESTAURANTS_NAME
-	STORAGE_RESTAURANTS_STREAMARN
+  ENV
+  REGION
+  STORAGE_RESTAURANTS_ARN
+  STORAGE_RESTAURANTS_NAME
+  STORAGE_RESTAURANTS_STREAMARN
 Amplify Params - DO NOT EDIT */
 
-/**
- * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
- */
+const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+const { DynamoDBDocumentClient, ScanCommand } = require("@aws-sdk/lib-dynamodb");
+
 exports.handler = async (event) => {
-    console.log(`EVENT: ${JSON.stringify(event)}`);
+  const client = new DynamoDBClient({});
+  const docClient = DynamoDBDocumentClient.from(client);
+
+  if (!process.env.STORAGE_RESTAURANTS_NAME) {
     return {
-        statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  },
-        body: JSON.stringify('Hello from Lambda!'),
+      statusCode: 500,
+      body: JSON.stringify({ message: "No restaurants found" }),
     };
+  }
+
+  try {
+    const { Items } = await docClient.send(
+      new ScanCommand({ TableName: process.env.STORAGE_RESTAURANTS_NAME })
+    );
+
+    if (!Items || Items.length === 0) {
+      return {
+        statusCode: 404,
+        body: JSON.stringify({ message: "No Item found" }),
+      };
+    }
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(Items),
+    };
+  } catch (error) {
+    console.error("DynamoDB scan error:", error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: "Internal Server Error" }),
+    };
+  }
 };
--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,27 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "yelpapi": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "getRestaurantReviews": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
+    "getRestaurants": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
   "storage": {
     "restaurants": {
       "Arn": "string",
